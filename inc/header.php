c823fffd78ded0ab359c4219c71a5646ed7918a8
antam0010@gmail.com <antam0010@gmail.com>
2022-11-28 20:59:32 +0900

    Initial commit

---
+++ inc/header.php
@@ -0,0 +1,46 @@
+<header id="header">
+    <div class="inner">
+
+        <!-- Logo -->
+        <a href="/" class="logo">
+            <span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title">486diary</span>
+        </a>
+
+        <!-- Nav -->
+        <nav>
+            <ul>
+                <li><a href="#menu">Menu</a></li>
+            </ul>
+        </nav>
+
+        <!-- Menu -->
+		<nav id="menu">
+			<h2>Menu</h2>
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/gallery.php">Gallery</a></li>
+				<li><a href="/news.php">News</a></li>
+				<li><a href="generic.php">Consequat dolor</a></li>
+				<li><a href="elements.html">Elements</a></li>
+			</ul>
+		</nav>
+
+    </div>
+</header>
+
+
+<!-- Scripts -->
+<script src="/assets/js/axios.min.js"></script>
+<script src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/browser.min.js"></script>
+<script src="/assets/js/breakpoints.min.js"></script>
+<script src="/assets/js/util.js"></script>
+<script src="/assets/js/main.js"></script>
+<script src="/assets/js/vue.js"></script>
+<script>
+    axios.defaults.baseURL = 'http://api.486diary.com'
+</script>
+
+
+
+

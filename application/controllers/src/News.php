fe1c3680e4cefe2b00888282f1caf3340c91db25
antam0010@gmail.com <antam0010@gmail.com>
2022-11-29 23:06:19 +0900

    news 추가

---
+++ application/controllers/src/News.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class News extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+    public function list() {
+        $this->load->view('/src/news/list');
+    }
+
+    public function write() {
+        $this->load->view('/src/news/write');
+    }
+
+}
---
+++ application/views/src/news/list.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>News</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="/assets/css/main.css" />
+    <noscript>
+        <link rel="stylesheet" href="/assets/css/noscript.css" />
+    </noscript>
+</head>
+
+<body class="is-preload">
+    <!-- Wrapper -->
+    <div id="wrapper">
+        <!-- Header -->
+        <? include_once $_SERVER['DOCUMENT_ROOT'] . '/inc/header.php' ?>
+
+        <!-- Main -->
+        <div id="app">
+            <div id="main">
+                <div class="inner">
+                    <h1>새소식</h1>
+                    <section>
+                        <div class="table-wrapper">
+                            <table>
+                                <thead>
+                                <colgroup>
+                                    <col width="10%" />
+                                    <col width="*" />
+                                    <col width="20%" />
+                                </colgroup>
+                                    <tr>
+                                        <th>작성자</th>
+                                        <th>제목</th>
+                                        <th>등록일</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-if="total>0" v-for="(row,i) in list">
+                                        <td>{{ row.member_nickname }}</td>
+                                        <td class="over_char">{{ row.board_title }}</td>
+                                        <td>{{ row.board_wdate }}</td>
+                                    </tr>
+                                    
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td colspan="2">총 게시물 <d/td>
+                                        <td>{{ total }}</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+        </div>
+        <!-- Footer -->
+        <? include_once $_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php' ?>
+
+
+    </div>
+</body>
+
+</html>
+
+
+<script>
+    const vm = new Vue({
+        // vm : view model의 약자
+        el: "#app",
+        data() {
+            return {
+                list: [],
+                total: 0,
+            }
+        },
+        mounted() {
+            this.doSearch();
+        },
+        methods: {
+            doSearch() {
+                let me = this;
+                let info = {
+                    'board_category': 'news',
+                    'pageSize': 5
+                }
+                axios.get('/board/list', { params: info }).then(response => {
+                    me.list = response.data.list
+                    me.total = response.data.total
+                });
+            }
+
+        },
+    });
+</script>

873539b74821589b8f6f63a655e07bb58f39cce7
antam2010 <antam2010@naver.com>
2023-12-23 18:39:48 +0900

    config 수정

--- application/config/config.php
+++ application/config/config.php
@@ -1,17 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-switch ($_SERVER['HTTP_HOST']) {
-    case 'daeho.shop':
-        $config['base_url'] = 'http://daeho.shop';
-        break;
-    case 'daeho.store':
-        $config['base_url'] = 'http://daeho.store';
-        break;
-    default:
-        $config['base_url'] = 'http://local.486diary.shop'; 
-        break;
-}
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
